02f36c0adb2f9d7753b03d848b4af317902e4a19
Matvey Kondratev <kondratev.matvei@yandex.ru>
2023-06-21 15:00:26 +0300

    complete task syntaxis

--- syntaxis/my_class/main.py
+++ syntaxis/my_class/main.py
@@ -2,8 +2,28 @@ from test_classes import *
 
 
 def main():
-    car = Test()
-    print(car.my_color())
+    my_animal = Dog(say="Whow!", color='white',
+                    names='sharik', )
+    show(my_animal)
+
+
+def show_info(animal):
+    for i in animal.info_animal().items():
+        print(f'{i[0]}: {i[1]}')
+        yield i
+
+
+def show(animals):
+    i = 0
+    a = show_info(animals)
+    print("Input 'n' to continue: ")
+    while i != 3:
+        my_input = input()
+        if my_input == 'n':
+            next(a)
+            i += 1
+        else:
+            print('Incorrect input, try again')
 
 
 if "__main__" == __name__:
--- syntaxis/my_class/test_classes.py
+++ syntaxis/my_class/test_classes.py
@@ -1,14 +1,30 @@
-class Test:
-    __color = 'Red'
+class Animal:
 
-    def turn_on(self):
-        pass
+    def __init__(self, environment=None):
+        self._environment = environment
 
-    def call(self):
-        pass
+    def info_animal(self):
+        return self._environment
 
-    def my_color(self):
-        return self.__color
+
+class Dog(Animal):
+
+    def __init__(self,*, say=None, color=None, names=None):
+        super().__init__()
+        self._say = say
+        self._color = color
+        self._name = names
+
+    def do_say(self):
+        print(self._say)
+
+    def get_name(self):
+        return self._name
+
+    def info_animal(self):
+        return {'name': self._name,
+                'how_say': self._say,
+                'color': self._color}
 
 
 if '__main__' == __name__:
