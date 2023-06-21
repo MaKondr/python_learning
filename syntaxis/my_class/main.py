10fb9d9fda732c20f6cd6bbd7c441ac4bb4f2734
Matvey Kondratev <kondratev.matvei@yandex.ru>
2023-06-21 13:27:19 +0300

    add task my_class

---
+++ syntaxis/my_class/main.py
@@ -0,0 +1,10 @@
+from test_classes import *
+
+
+def main():
+    car = Test()
+    print(car.my_color())
+
+
+if "__main__" == __name__:
+    main()
---
+++ syntaxis/my_class/test_classes.py
@@ -0,0 +1,15 @@
+class Test:
+    __color = 'Red'
+
+    def turn_on(self):
+        pass
+
+    def call(self):
+        pass
+
+    def my_color(self):
+        return self.__color
+
+
+if '__main__' == __name__:
+    print('Ты чего наделал?!')
